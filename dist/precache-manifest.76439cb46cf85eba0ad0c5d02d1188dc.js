596fed7891791b9f168048cf96f47b21b7e683ea
Carofif <carollebonou@gmail.com>
2019-10-10 10:02:43 +0100

    initialisation

---
+++ dist/precache-manifest.76439cb46cf85eba0ad0c5d02d1188dc.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0d13d16cc2f3647680d9f1ff003f58b",
+    "url": "/fonts/materialdesignicons-webfont.a0d13d16.woff"
+  },
+  {
+    "revision": "b76eb8dd473607290ea874f8ef33983c",
+    "url": "/data-sources/avatars/nicolas-horn-689011-unsplash.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "146059c0e0fc92c68594",
+    "url": "/js/chunk-vendors.8c569135.js"
+  },
+  {
+    "revision": "40dfca04512f89fd742f",
+    "url": "/js/client-form.f5e241e2.js"
+  },
+  {
+    "revision": "1baa860007d98bd37a1c",
+    "url": "/js/client-form~forms.b032b80b.js"
+  },
+  {
+    "revision": "6fb1180b5517b1ad0363",
+    "url": "/js/forms.c539b085.js"
+  },
+  {
+    "revision": "a3223266d07a462be2f3",
+    "url": "/js/profile.e4acbdda.js"
+  },
+  {
+    "revision": "b7a1404bd639fbaf1854",
+    "url": "/js/tables.31349b4c.js"
+  },
+  {
+    "revision": "7298fabbecbc6f87cf690f717af8652c",
+    "url": "/img/justboil-logo.7298fabb.svg"
+  },
+  {
+    "revision": "7ec5dab7e7ff250971d2ff50379778dc",
+    "url": "/fonts/materialdesignicons-webfont.7ec5dab7.woff2"
+  },
+  {
+    "revision": "ff5d6a007300919bce0f",
+    "url": "/js/app.cea334ba.js"
+  },
+  {
+    "revision": "a32fa1f27abbfa96ff2f79e1ade723d5",
+    "url": "/fonts/materialdesignicons-webfont.a32fa1f2.eot"
+  },
+  {
+    "revision": "3ac50b5b36eb2f11b000dce1792d0bb0",
+    "url": "/fonts/materialdesignicons-webfont.3ac50b5b.ttf"
+  },
+  {
+    "revision": "62cd4025a0ae1e1c682c03ff14450bec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63e8d58029d95b6ea85c0a92e24eca83",
+    "url": "/data-sources/clients.json"
+  },
+  {
+    "revision": "924a58fb4e40f8bec6d5eabce2947744",
+    "url": "/data-sources/avatars/joshua-rawson-harris-432289-unsplash.jpg"
+  },
+  {
+    "revision": "16a2e6c8d9505fd5d3cd9a42362f3ddc",
+    "url": "/data-sources/avatars/freestocks-org-190062-unsplash.jpg"
+  },
+  {
+    "revision": "48f7f75c2e775c5004ce0886e5e6c5e2",
+    "url": "/data-sources/avatars/benjamin-parker-699522-unsplash.jpg"
+  },
+  {
+    "revision": "a6f1f670e35e2b4521eeb0c04de5b4c2",
+    "url": "/data-sources/avatars/annie-spratt-121576-unsplash.jpg"
+  },
+  {
+    "revision": "e6b7e524fc15f61b8033c382f1f7e0da",
+    "url": "/data-sources/avatars/andy-lee-642320-unsplash.jpg"
+  },
+  {
+    "revision": "146059c0e0fc92c68594",
+    "url": "/css/chunk-vendors.42858530.css"
+  },
+  {
+    "revision": "ff5d6a007300919bce0f",
+    "url": "/css/app.e4c9a1a6.css"
+  }
+];
